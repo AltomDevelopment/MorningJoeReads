36eeaefcf698678703bad6664b7846cae6c0cf26
Zachery.Altom <zack.altom@outlook.com>
2018-12-01 14:13:10 -0500

    Added some basic Layout pages based on diagrams

--- code/MorningJoeReadsWebUI/Controllers/LoggedInHomeController.cs
+++ code/MorningJoeReadsWebUI/Controllers/LoggedInHomeController.cs
@@ -6,13 +6,12 @@ using System.Web.Mvc;
 
 namespace MorningJoeReadsWebUI.Controllers
 {
-    public class HomeController : Controller
+    public class LoggedInHomeController : Controller
     {
+        // GET: LoggedInHome
         public ActionResult Index()
         {
             return View();
         }
-
-        
     }
 }
