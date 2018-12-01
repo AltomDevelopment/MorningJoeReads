e639503bb1e2a82cd6932708160d43cbe8401f34
Zachery.Altom <zack.altom@outlook.com>
2018-12-01 17:03:20 -0500

    Added functionality to save login data to database

--- code/MorningJoeReadsWebUI/Controllers/LoginController.cs
+++ code/MorningJoeReadsWebUI/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MorningJoeReadsWebUI.Context;
 using MorningJoeReadsWebUI.Models;
 using MorningJoeReadsWebUI.ViewModel;
 
@@ -21,16 +22,17 @@ namespace MorningJoeReadsWebUI.Controllers
         [HttpPost]
         public ActionResult Index(LoginViewModel loginViewModel)
         {
-            if (ModelState.IsValid)
-            {
-                User user = new User();
-                user.FirstName = loginViewModel.FirstName;
-                user.LastName = loginViewModel.LastName;
-                user.EmailAddress = loginViewModel.EmailAddress;
-                user.PassWord = loginViewModel.PassWord;
-                
-            }
+            DomainContext db = new DomainContext();
 
+            User user = new User();
+            user.FirstName = loginViewModel.FirstName;
+            user.LastName = loginViewModel.LastName;
+            user.EmailAddress = loginViewModel.EmailAddress;
+            user.PassWord = loginViewModel.PassWord;
+
+            db.Users.Add(user);
+            db.SaveChanges();
+     
             return View();
         }
     }
