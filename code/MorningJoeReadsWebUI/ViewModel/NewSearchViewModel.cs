1f2a44ff5715c83ee4e28e9d03ab6309a72b4216
Zachery.Altom <zack.altom@outlook.com>
2018-12-07 10:50:34 -0500

    Added Jquery script to test add button functionality

--- code/MorningJoeReadsWebUI/ViewModel/NewSearchViewModel.cs
+++ code/MorningJoeReadsWebUI/ViewModel/NewSearchViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MorningJoeReadsWebUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,5 +10,6 @@ namespace MorningJoeReadsWebUI.ViewModel
     {
         public string SearchDescription { get; set; }
         public string SearchName { get; set; }
+        public List<Search> Searches = new List<Search>();
     }
 }
